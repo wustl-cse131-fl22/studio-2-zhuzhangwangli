9d6a8b27f72ba82d0f6ff9170b4b99623a3c4490
livictoriaaa <li.victoria@wustl.edu>
2023-10-12 14:41:55 -0500

    commit

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -6,54 +6,43 @@ public class Ruin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.print("Start Amount: ");
 		double startAmount = scan.nextDouble();
-		
-		System.out.println("Win Limit: ");
+
+		System.out.print("Win Limit: ");
 		double winLimit = scan.nextDouble();
-		
-		System.out.println("Win Chance: ");
+
+		System.out.print("Win Chance: ");
 		double winChance = scan.nextDouble();
 		int a = 0;
-		 
-		for (int k = 0; k <= 500; k ++)
-		{int t = 0;
-		double startMoney = startAmount;
-		while (startMoney > 0 && startMoney <= winLimit) {
-		boolean winChanceBo = Math.random() <= winChance;
-		if(winChanceBo)
-		{
-		startMoney ++;
-		}
-		else
-		{
-		startMoney --;
-		}
-		t++;
-		
-		
-		
-		
-		
-		
-		}
-		if (startMoney == winLimit+1) {
-			System.out.println("Simulation " + k + ": "+(t-1)+" WIN");
-		}
-		else {
-			System.out.println("Simulation " + k + ": "+t+" LOSSE");
-			a++;
-		}
+
+		for (int k = 0; k <= 500; k++) {
+			int t = 0;
+			double startMoney = startAmount;
+			while (startMoney > 0 && startMoney <= winLimit) {
+				boolean winChanceBo = true;
+				winChanceBo = Math.random() <= winChance;
+				if (winChanceBo) {
+					startMoney++;
+				} else {
+					startMoney--;
+				}
+				t++;
+
+			}
+			if (startMoney == winLimit + 1) {
+				System.out.println("Simulation " + k + ": " + (t - 1) + " WIN");
+			} else {
+				System.out.println("Simulation " + k + ": " + t + " LOSS");
+				a++;
+			}
 //		System.out.println("money left: " + startMoney);
-		
-		
-		
-		
-	}
-		System.out.println("Losses: "+ a + " Simulations: 500");
+
+		}
+		System.out.println("Losses: " + a + " Simulations: 500");
 
 	}
-} 
+}
